36901281944ead430845ab8bf4daa9874bd8d20f
SisaNjitap <ntakpestephane@hotmail.fr>
2019-03-19 23:43:07 +0100

    Oui

---
+++ OldComment.php
@@ -0,0 +1,7 @@
+<div class="old">
+<div class="titre"> Forum
+ </div>
+<div class="nom"> Stéphane </div>
+<div class="question"> Pourquoi Karaba la sorcière est elle méchante </div>
+<div class="OldComment"> </div>
+</div>
---
+++ commentaires.php
@@ -0,0 +1,4 @@
+<div class="commentaires">
+<div class="pseudo"> Choix du pseudo </div>
+<div class="commentaire"> Commentaire </div>
+</div>
