03c1f1a46ba65427f7cfd43edb2a405520741b51
Thibtoy <cabanes.thibault@gmail.com>
2019-03-20 11:51:42 +0100

    le post

---
+++ header.php
@@ -0,0 +1,6 @@
+<header class="header"> 
+<div class ="accueil"> Accueil </div>  <br /> 
+<div class ="profil"> Profil </div>   <br /> 
+<div class ="sujets"> Sujets </div> <br /> 
+<div class ="contacts"> Contacts </div>  <br /> 
+</header>
---
+++ test.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link href="https://fonts.googleapis.com/css?family=Gloria+Hallelujah" rel="stylesheet">
+<link rel="stylesheet" href="test.css" />
+    <title>Test</title>
+</head>
+
+<body>
+<?php include("header.php") ?>
+<?php include("footer.php") ?>
+<?php include("OldComment.php") ?>
+<?php include("commentaires.php") ?>
+
+</body>
+
+</html> 
